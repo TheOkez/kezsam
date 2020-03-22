a876cc19d5451d8b9e8128b5a4c3970bb21e4dda
TheOkez <kezsam52@gmail.com>
2020-03-22 10:48:41 +0100

    js script

---
+++ JSTask.js
@@ -0,0 +1,11 @@
+let myName = "Udo, Okezie Samuel";
+let myCourses =["HTML", "CSS" ,"JS" ,"Design"];
+console.log(myName);
+console.log(myCourses);
+
+//myCourses is Even
+for (let i = 1; i <= 200; i++) {
+	if (i % 2 === 0) {
+		console.log(i);
+	}
+}
